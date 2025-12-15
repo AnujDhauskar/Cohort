e3c690955676772b3bf9f4291215fe28ca07331c
Anuj Dhauskar <anujdhauskar64@gmail.com>
2025-12-15 12:58:59 +0530

    WeatherAPI Scenerio1

---
+++ WeatherAPI callig/script.js
@@ -0,0 +1,21 @@
+// let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}`;
+
+async function getWeather(city) {
+try {
+    let apikey = `8f81216b27b979bac1531a19420eaa81`;
+
+    let raw = await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}`
+    );
+
+    if (!raw.ok) {
+    throw new Error("City not found, Something went wrong");
+    }
+    let real = await raw.json();
+    console.log(real);
+} catch (err) {
+    console.log(err.message);
+}
+}
+
+getWeather("pune");
